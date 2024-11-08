8036dc379fb9f01313c38c98017a6500c52791ed
Kevin Netherton <kevin.netherton@gov.bc.ca>
2024-11-08 08:01:16 -0800

    feat: script to run the spar ingest from docker

---
+++ data_prep/docker_run_SPAR_TEST.bash
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+# wrapper script to initiate the data load
+echo "install poetry"
+python3 -m pip install poetry
+cd /application
+poetry install
+source $(poetry env info --path)/bin/activate
+# echo "activate the venv"
+# source /venv/bin/activate
+echo "run the data ingestion"
+python3 ./main_spar_ingest.py TEST
