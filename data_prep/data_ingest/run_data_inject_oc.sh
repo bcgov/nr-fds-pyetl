d7bdb7a0507861c6c292812f18615ae04b052381
Kevin Netherton <kevin.netherton@gov.bc.ca>
2025-06-19 08:41:53 -0700

    feat: configure the docker run to run under regular user

---
+++ data_prep/data_ingest/run_data_inject_oc.sh
@@ -0,0 +1,10 @@
+# chown -R derek:derek /app/data
+# exec gosu derek "$@"
+echo dc ss is: $DATA_CLASSIFICATION_SS
+
+if [ "$(id -u)" = "0" ]; then
+  chown -R derek:derek /app/data
+  exec gosu derek python db_env_utils/main_ingest.py OC_POSTGRES PROD
+else
+  exec python db_env_utils/main_ingest.py OC_POSTGRES PROD
+fi
